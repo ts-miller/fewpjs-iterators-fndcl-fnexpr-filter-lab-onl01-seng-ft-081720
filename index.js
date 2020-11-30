1c90072449589cccd64795ca392fba4731eed295
ts-miller <tylerstephen11@gmail.com>
2020-11-29 19:27:27 -0500

    commit tree dbba187d427157c8e32830c403020b6d0fdeb9a2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(n => {
+        return n.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(n => {
+        return n.startsWith(`${string}`)
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(n => {
+        return n['name'] === string
+    })
+}
